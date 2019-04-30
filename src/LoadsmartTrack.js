a3d3b59b4b39535209a5262e21f67a6a18a67a6e
Otavio Augusto <tavioalves@gmail.com>
2019-04-30 16:06:32 -0300

    clean: Remove unnecessary files from the project

---
+++ src/LoadsmartTrack.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class LoadsmartTrack extends Component {
+  render() {
+    return (
+      <div>
+        <nav>Loadsmart</nav>
+      </div>
+    );
+  }
+}
