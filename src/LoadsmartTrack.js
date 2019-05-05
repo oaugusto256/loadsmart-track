ae545d672693f01fd722330e684e77f9f90a3a93
Otavio Augusto <tavioalves@gmail.com>
2019-05-05 13:11:22 -0300

    feat: Create the Navbar component

--- src/LoadsmartTrack.js
+++ src/LoadsmartTrack.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
 
-import logo from './images/logo.png';
-
 import './styles/index.scss';
+import Navbar from './components/Navbar';
 
 export default class LoadsmartTrack extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar">
-          <div className="container">
-            <div className="logo">
-              <img className="img" src={logo} alt="Loadsmart Logo" />
-            </div>
-            <div className="menu">
-              <div className="item">(646) 887 6278</div>
-              <div className="item">Shipper</div>
-              <div className="item">Become a Carrier</div>
-              <div className="item">Login</div>
-              <div className="item">Sign Up </div>
-            </div>
-          </div>
-        </nav>
-      </div>
+      <>
+        <Navbar />
+      </>
     );
   }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../images/logo.png';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img className="img" src={logo} alt="Loadsmart Logo" />
+        </div>
+        <div className="menu">
+          <div className="item">(646) 887 6278</div>
+          <div className="item">Shipper</div>
+          <div className="item">Become a Carrier</div>
+          <div className="item">Login</div>
+          <div className="item">Sign Up </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
